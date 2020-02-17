b6e1c67ecd932994088189a741b99cd98ac325f3
Josh Garcia <josh2397garcia@gmail.com>
2020-02-17 11:58:32 -0500

    Handle Checkout and ContactData in redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -45,7 +44,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-            this.setState({purchaseable: sum > 0});
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -57,19 +56,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        this.props.history.push('/checkout');
     }
 
     render () {
@@ -91,7 +78,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemove}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler} />
                 </Auxiliary>
             );
